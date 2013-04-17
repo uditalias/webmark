49542a776664459b8888c2be1ff803aeab52a2af
Udi Talias <udi.talias@gmail.com>
2013-04-17 14:51:24 +0300

    Update website

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -45,7 +45,7 @@ function onScroll(e){
 }
 
 function generateRandomColor(){
-	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+	return '#' + ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
 }
 
 function calculateOptimalCirclesCount(){
