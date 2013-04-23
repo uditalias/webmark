aef4934ec1461cd96d611263dc30ef719cdfc160
Udi Talias <udi.talias@gmail.com>
2013-04-23 11:01:18 +0300

    Update website

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,58 +1,60 @@
-var circleRows = 6;
-var circleWidth = 54;
-var circlesCount = 0;
-var circles = [];
-var circlesInterval = null;
+(function(){
+	var circleRows = 6;
+	var circleWidth = 54;
+	var circlesCount = 0;
+	var circles = [];
+	var circlesInterval = null;
 
-function initPage(){
-	initCircles();
-}
+	function initPage(){
+		initCircles();
+	}
 
-function initCircles(){
-	circlesCount = calculateOptimalCirclesCount();
-	if(circlesInterval){
-		window.clearInterval(circlesInterval);
-		circlesInterval = null;
-		if(circles.length){
-			for(var i = 0, len = circles.length; i < len; i++){
-				document.getElementById('background').removeChild(circles[i]);
+	function initCircles(){
+		circlesCount = calculateOptimalCirclesCount();
+		if(circlesInterval){
+			window.clearInterval(circlesInterval);
+			circlesInterval = null;
+			if(circles.length){
+				for(var i = 0, len = circles.length; i < len; i++){
+					document.getElementById('background').removeChild(circles[i]);
+				}
 			}
+			circles = [];
 		}
-		circles = [];
+		for(var i = 0; i < circlesCount; i++){
+			var circle = document.createElement('div');
+			circle.className = 'circle';
+			circle.style.backgroundColor = generateRandomColor();
+			document.getElementById('background').appendChild(circle);
+			circles.push(circle);
+		}
+
+		circlesInterval = window.setInterval(function(){
+			var rndIndex = Math.floor(Math.random() * circlesCount);
+			circles[rndIndex].style.backgroundColor = generateRandomColor();
+		}, 100);
+
+		document.getElementsByTagName('section')[0].style.marginLeft = 0;
 	}
-	for(var i = 0; i < circlesCount; i++){
-		var circle = document.createElement('div');
-		circle.className = 'circle';
-		circle.style.backgroundColor = generateRandomColor();
-		document.getElementById('background').appendChild(circle);
-		circles.push(circle);
+
+	function onScroll(e){
+		if(document.body.scrollTop > 15){
+			document.getElementsByTagName('nav')[0].style.boxShadow = '-5px 2px 2px rgba(0,0,0,.4)';
+		} else {
+			document.getElementsByTagName('nav')[0].style.boxShadow = '';
+		}
 	}
 
-	circlesInterval = window.setInterval(function(){
-		var rndIndex = Math.floor(Math.random() * circlesCount);
-		circles[rndIndex].style.backgroundColor = generateRandomColor();
-	}, 100);
-
-	document.getElementsByTagName('section')[0].style.marginLeft = 0;
-}
-
-function onScroll(e){
-	if(document.body.scrollTop > 15){
-		document.getElementsByTagName('nav')[0].style.boxShadow = '-5px 2px 2px rgba(0,0,0,.4)';
-	} else {
-		document.getElementsByTagName('nav')[0].style.boxShadow = '';
+	function generateRandomColor(){
+		return '#' + ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
 	}
-}
 
-function generateRandomColor(){
-	return '#' + ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
-}
+	function calculateOptimalCirclesCount(){
+		var perRow = Math.floor(document.width / circleWidth);
+		return perRow * circleRows;
+	}
 
-function calculateOptimalCirclesCount(){
-	var perRow = Math.floor(document.width / circleWidth);
-	return perRow * circleRows;
-}
-
-window.onload = initPage;
-window.onresize = initCircles;
-window.onscroll = onScroll;
+	window.onload = initPage;
+	window.onresize = initCircles;
+	window.onscroll = onScroll;
+})();
